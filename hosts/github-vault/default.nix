547623b6a4f3184768d59771f4dfae1ae0a676f6
madebycli <root.cli@proton.me>
2026-08-05 20:42:00 +0200

    Correct pinned r8712u source hash

--- hosts/github-vault/default.nix
+++ hosts/github-vault/default.nix
@@ -10,7 +10,7 @@ let
     owner = "gregkh";
     repo = "linux";
     rev = "2b9719ccad38dffad7dbdd2f39896f723f9b9011";
-    hash = "sha256-jcYe540LHHAqgNRTWDuUac1AcglsW78Ps3SYyH6mLIE=";
+    hash = "sha256-zZKDLUqGYs0R/Ceb2JfN8QiEaCNKZML4rJMplhAOcmU=";
   };
 
   r8712uModule = pkgs.stdenv.mkDerivation {
